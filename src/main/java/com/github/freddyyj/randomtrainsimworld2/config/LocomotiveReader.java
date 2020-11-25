9e9dd5a29fefe3e84daa1031ab29dc2193ba9be7
김영재 <kyj141100@naver.com>
2020-11-25 14:00:12 +0900

    Add LocomotiveReader

---
+++ src/main/java/com/github/freddyyj/randomtrainsimworld2/config/LocomotiveReader.java
@@ -0,0 +1,63 @@
+package com.github.freddyyj.randomtrainsimworld2.config;
+
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class LocomotiveReader {
+    private static File file=new File(Thread.currentThread().getContextClassLoader().getResource("locomotives.json").getFile());
+    private static ArrayList<LocomotiveReader> routes=new ArrayList<>();
+    private String code;
+    private String name;
+    private List<String> locomotives;
+    private String nation;
+    private LocomotiveReader(String code, String name, List<String> locos,String nation){
+        this.code=code;
+        this.name=name;
+        this.nation=nation;
+        locomotives=locos;
+    }
+    public static void reload(){
+        try {
+            JsonReader reader= Json.createReader(new FileInputStream(file));
+            JsonObject object=reader.readObject();
+            ArrayList<String> locos=new ArrayList<>();
+
+            Set<String> set=object.keySet();
+            for (int i=0;i< set.size();i++){
+                locos.clear();
+                String key=(set.toArray(new String[1]))[i];
+                JsonObject route=object.getJsonObject(key);
+
+                route.getJsonArray("locomotives").forEach(locomotive->{
+                    locos.add(locomotive.toString());
+                });
+                routes.add(new LocomotiveReader(key,route.getString("name"),locos,route.getString("nation")));
+            }
+
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static List<LocomotiveReader> getLocomotiveReaders(){return routes;}
+    public static LocomotiveReader getLocomotiveReader(String code){
+        for (int i=0;i<routes.size();i++){
+            if (routes.get(i).getCode().equals(code)) return routes.get(i);
+        }
+        return null;
+    }
+    public String getCode(){return code;}
+    public String getName(){return name;}
+    public String getNation(){return nation;}
+    public List<String> getLocomotives(){return locomotives;}
+}
--- src/test/java/IOStreamTest.java
+++ src/test/java/IOStreamTest.java
@@ -1,3 +1,4 @@
+import com.github.freddyyj.randomtrainsimworld2.config.LocomotiveReader;
 import com.github.freddyyj.randomtrainsimworld2.config.WeatherReader;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,7 +8,9 @@ import java.util.List;
 public class IOStreamTest {
     @Test
     public void readLocomotiveTest(){
+        LocomotiveReader.reload();
 
+        Assert.assertEquals(LocomotiveReader.getLocomotiveReader("NTP").getName(),"Northern-Trans Pennine");
     }
     @Test
     public void readWeatherTest(){
