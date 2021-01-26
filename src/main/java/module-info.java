b7e0a84e186f8a4a36e41d89259dad36d40dd4b0
FreddyYJ <kyj-1411@hanmail.net>
2021-01-26 11:37:45 +0900

    Add module-info.json

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * RandomTSW2 is random picker for Train Sim World 2.
+ * @author FreddyYJ_
+ */
+module RandomTrainSimWorld2.main {
+    exports com.github.freddyyj.randomtrainsimworld2;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+}
