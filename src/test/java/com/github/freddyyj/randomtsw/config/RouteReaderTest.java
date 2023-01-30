2ba3908f1aa9096cee601a18fe9f075af1eb9e99
FreddyYJ <kyj1411@unist.ac.kr>
2023-01-30 21:32:23 +0900

    Add Edinburgh Glasgow

--- src/test/java/com/github/freddyyj/randomtsw/config/RouteReaderTest.java
+++ src/test/java/com/github/freddyyj/randomtsw/config/RouteReaderTest.java
@@ -23,7 +23,7 @@ public class RouteReaderTest {
     @Test
     public void getRouteReadersTest(){
         List<RouteReader> readers= RouteReader.getRouteReaders();
-        Assertions.assertEquals(readers.size(),42);
+        Assertions.assertEquals(readers.size(),43);
     }
 
     @Test
