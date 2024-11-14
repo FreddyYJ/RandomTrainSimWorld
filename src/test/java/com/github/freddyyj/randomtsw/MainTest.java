3925c6afbb07ad4c28da420a9d863d95b7e5828c
FreddyYJ <kyj1411@unist.ac.kr>
2024-11-14 20:01:00 +0900

    Update to 1.1.2

--- src/test/java/com/github/freddyyj/randomtsw/MainTest.java
+++ src/test/java/com/github/freddyyj/randomtsw/MainTest.java
@@ -21,7 +21,7 @@ public class MainTest {
     @Test
     @Order(2)
     public void getRoutesTest() {
-        Assertions.assertEquals(core.getRoutes().size(),63);
+        Assertions.assertEquals(core.getRoutes().size(),64);
     }
     @Test
     @Order(2)
--- src/test/java/com/github/freddyyj/randomtsw/config/RouteReaderTest.java
+++ src/test/java/com/github/freddyyj/randomtsw/config/RouteReaderTest.java
@@ -23,7 +23,7 @@ public class RouteReaderTest {
     @Test
     public void getRouteReadersTest(){
         List<RouteReader> readers= RouteReader.getRouteReaders();
-        Assertions.assertEquals(readers.size(),63);
+        Assertions.assertEquals(readers.size(),64);
     }
 
     @Test
